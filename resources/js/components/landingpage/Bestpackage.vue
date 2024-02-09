8062eeb8f4dec257ff3eb43adcd5afd6934c41f3
samalade11 <samuel.alade@delwathon.com>
2024-02-09 19:04:33 +0100

    landing page

--- resources/js/components/landingpage/Bestpackage.vue
+++ resources/js/components/landingpage/Bestpackage.vue
@@ -149,6 +149,100 @@
                     </div>
                 </div>
             </div>
+
+
+            <div class="mb-20">
+                <h4 class="text-[#1B1B1B] font-medium text-[15px] md:text-[30px] mb-6">Trips made easier for everyone, everywhere</h4>
+
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-4">
+                    <div class="w-fit relative ">
+                        <img src="../../../../public/assets/tripimg1.png" alt="">
+                        <div class=" w-fit absolute bottom-3 left-4">
+                            <p class="text-white font-medium text-[22px]">We book and you relax</p>
+                            <p class="text-white text-[15px] w-5/6 lg:w-3/4 my-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa est proin viverra et vitae magna mauris.</p>
+                            <a href="" class="text-white text-[15px] flex">
+                                <p class="uppercase font-medium underline">see hotels</p>
+                                <i class='bx bx-right-arrow-alt my-auto ml-1 font-medium' ></i>
+                            </a>
+                        </div>
+                    </div>
+    
+    
+                    <!-- 22 -->
+    
+                    <div class="w-fit relative ">
+                        <img src="../../../../public/assets/tripimg2.png" alt="">
+                        <div class=" w-fit absolute bottom-3 left-4">
+                            <p class="text-white font-medium text-[22px]">Travel Small Checklist</p>
+                            <p class="text-white text-[15px] w-5/6 lg:w-3/4 my-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa est proin viverra et vitae magna mauris.</p>
+                            <a href="" class="text-white text-[15px] flex">
+                                <p class="uppercase font-medium underline">learn more</p>
+                                <i class='bx bx-right-arrow-alt my-auto ml-1 font-medium' ></i>
+                            </a>
+                        </div>
+                    </div>
+    
+                    <!-- 333 -->
+    
+                    <div class="w-fit relative ">
+                        <img src="../../../../public/assets/tripimg3.png" alt="">
+                        <div class=" w-fit absolute bottom-3 left-4">
+                            <p class="text-white font-medium text-[22px]">Help from Expert Staff</p>
+                            <p class="text-white text-[15px] w-5/6 lg:w-3/4 my-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa est proin viverra et vitae magna mauris.</p>
+                            <a href="" class="text-white text-[15px] flex">
+                                <p class="uppercase font-medium underline">learn more</p>
+                                <i class='bx bx-right-arrow-alt my-auto ml-1 font-medium' ></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mb-20">
+                <h4 class="text-[#1B1B1B] font-medium text-[15px] md:text-[30px] mb-6">Vacation in simple steps</h4>
+
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 mx-4 mb-16 mt-8">
+                    <div class="">
+                        <div class="flex justify-between ">
+                            <p class="text-[#181C17] font-medium text-[18px] md:text-[24px] my-auto h-fit">Select a distination</p>
+                            <img src="../../../../public/assets/editicon.png" alt="" class="h-fit my-auto">
+                        </div>
+                        <p class="text-[#434642] text-[16px] w-3/4 ">Make updates to your itinerary or cancel a booking</p>
+                    </div>
+
+                    <div class="">
+                        <div class="flex justify-between ">
+                            <p class="text-[#181C17] font-medium text-[18px] md:text-[24px] my-auto h-fit">Make payment</p>
+                            <img src="../../../../public/assets/dollaricon.png" alt="" class="h-fit my-auto">
+                        </div>
+                        <p class="text-[#434642] text-[16px] w-3/4 ">Apply a coupon code or credit to a new trip</p>
+                    </div>
+
+                    <div class="">
+                        <div class="flex justify-between ">
+                            <p class="text-[#181C17] font-medium text-[18px] md:text-[24px] my-auto h-fit">Track your vacation request</p>
+                            <img src="../../../../public/assets/requesticon.png" alt="" class="h-fit my-auto">
+                        </div>
+                        <p class="text-[#434642] text-[16px] w-3/4 ">Check the status of a refund currently in progress</p>
+                    </div>
+                </div>
+
+                <div class="lg:flex gap-12">
+                    <div class="relative">
+                        <img src="../../../../public/assets/vacimg1.png" alt="">
+                        <div class=" absolute bottom-8 left-16">
+                            <p class="text-white font-medium text-[22px]">Become a Vendor</p>
+                            <p class="text-white text-[15px] w-5/6 lg:w-3/4 my-4">Introducing you to the choice of vendors for any of your destinations</p>
+                            <button class="text-sm text-white bg-[#1889FC] w-fit px-6 py-3 rounded-[7px]">
+                                Learn more
+                            </button>
+                        </div>
+                    </div>
+                    <div class="flex place-content-center mt-8 lg:mt-0">
+                        <img src="../../../../public/assets/vacimg2.png" alt="">
+                    </div>
+                </div>
+            </div>
        </div> 
     </div>
 </template>
