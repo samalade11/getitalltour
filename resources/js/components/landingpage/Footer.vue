9b6bcb888da6d1d1441ad705dc9d97a1b683e31f
samalade11 <samuel.alade@delwathon.com>
2024-02-09 20:00:23 +0100

    landing page

---
+++ resources/js/components/landingpage/Footer.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="mx-4 md:mx-8 lg:mx-24 mt-44 mb-24">
+        <div class=" grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mb-12 gap-12">
+            <div class=" relative">
+                <img src="../../../../public/assets/footerlogo.png" alt="">
+                <img src="../../../../public/assets/supportimg.png" alt="" class="bottom-0 absolute">
+            </div>
+            <div>
+                <p class="text-[#181C17] text-[26px] font-medium">Company</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-2">About</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-3">Jobs</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-3">List your property</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-3">Partnership</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-3">Newsroom</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-3">Partnership</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-3">Investor relation</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-3">Advertising</p>
+            </div>
+
+            <div>
+                <p class="text-[#181C17] text-[26px] font-medium">Explore</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-2">Hotels</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-3">Write a review</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-3">Add a place</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-3">Join</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-3">Traveller’s choice</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-3">Travel Insrance</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-3">Travel articles</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-3">Blog</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-3">Help center</p>
+            </div>
+
+            <div>
+                <p class="text-[#181C17] text-[26px] font-medium">Policies</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-2">Privacy policies</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-3">Term of use</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-3">Accessibility</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-3">Copyright</p>
+            </div>
+
+            <div>
+                <p class="text-[#181C17] text-[26px] font-medium">Help</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-2">Support</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-3">Cancel flight</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-3">Refund process</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-3 leading-5">Cancel your  hotel or<br>reservation bookings</p>
+                <p class="text-[#434642] text-[14px] font-medium mt-3">Use a coupon</p>
+            </div>
+        </div>
+
+        <hr class="border-b border-[#C9DBC9]">
+        <div class=" flex justify-between my-4">
+            <p class="text-[#434642] text-[12px] md:text-[16px]">&copy; 2024, Getitalltours, All rights reserved.</p>
+            <div class="flex gap-2 h-fit my-auto">
+                <img src="../../../../public/assets/fbicon.png" alt="">
+                <img src="../../../../public/assets/xicon.png" alt="">
+                <img src="../../../../public/assets/igicon.png" alt="">
+                <img src="../../../../public/assets/inicon.png" alt="">
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+
+</script>
--- resources/js/components/landingpage/Landingpage.vue
+++ resources/js/components/landingpage/Landingpage.vue
@@ -3,10 +3,12 @@
         <navbar />
         <Newdestination />
         <Bestpackage />
+        <Footer />
     </div>
 </template>
 <script setup>
 import navbar from './Navbar.vue';
 import Newdestination from './Newdestination.vue';
 import Bestpackage from './Bestpackage.vue';
+import Footer from './Footer.vue';
 </script>
